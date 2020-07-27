7b4c9ec87172b8584317f5dbd7c4ca9df116f9ec
urago-ms <urago@ms.esys.tsukuba.ac.jp>
2020-07-27 17:03:56 +0900

    [new file]generate model and execute task

---
+++ MATLAB-git/ninja_pick_and_place/spawn_and_grasp.m
@@ -0,0 +1,304 @@
+% This example illustrates how to execute complex commands from
+% a remote API client. You can also use a similar construct for
+% commands that are not directly supported by the remote API.
+%
+% Load the demo scene 'remoteApiCommandServerExample.ttt' in CoppeliaSim, then 
+% start the simulation and run this program.
+%
+% IMPORTANT: for each successful call to simxStart, there
+% should be a corresponding call to simxFinish at the end!
+
+function spawn_test()
+
+    disp('Program started');
+    % sim=remApi('remoteApi','extApi.h'); % using the header (requires a compiler)
+    sim=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+    sim.simxFinish(-1); % just in case, close all opened connections
+    clientID=sim.simxStart('127.0.0.1',19999,true,true,5000,5);
+
+    if (clientID>-1)
+        disp('Connected to remote API server');
+        
+% % %         Repeated creation and deletion of the device.
+        for count = 1:5
+
+%{            
+% % % % % %             Calling a function in the threaded child script of ResizableFloor_5_25 to create a facility
+            % % % Genetate a manipulator
+            [res retInts retFloats retStrings retBuffer]=sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'spawn_function', ...
+                [],[1.5,1.4,0], ...
+                'Hello world!', ...
+                [], ...
+                sim.simx_opmode_blocking);
+            %     pause(3);
+            if (res==sim.simx_return_ok)
+                fprintf('Returned message: %s\n',retStrings);
+            else
+                fprintf('Remote function call failed\n');
+            end
+            
+            % % % Genetate a conveyor
+            [res retInts retFloats retStrings retBuffer]=sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'spawn_function_con', ...
+                [0,0,1],[0.1,0.3,0.68], ...
+                'Hello world!', ...
+                [], ...
+                sim.simx_opmode_blocking);
+            %         pause(3);
+            
+            if (res==sim.simx_return_ok)
+                fprintf('Returned message: %s\n',retStrings);
+            else
+                fprintf('RRRRRRRRRemote function call failed\n');
+            end
+            
+            % % % Genetate a table
+            [res retInts retFloats retStrings retBuffer]=sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'spawn_function_tab', ...
+                [],[-1.0,1.0,0.68], ...
+                'Hello world!', ...
+                [], ...
+                sim.simx_opmode_blocking);
+            pause(1);
+            
+            
+            if (res==sim.simx_return_ok)
+                fprintf('Returned message: %s\n',retStrings);
+            else
+                fprintf('tttttttttttRRRemote function call failed\n');
+            end
+            
+            % 4. First send a command to display a specific message in a dialog box:
+            %         [res retInts retFloats retStrings retBuffer]=sim.simxCallScriptFunction(clientID, ...
+            %                                                                                 'ResizableFloor_5_25', ...
+            %                                                                                 sim.sim_scripttype_childscript, ...
+            %                                                                                 'remove_function_rob', ...
+            %                                                                                 [],[-1.0,1.0,0.68], ...
+            %                                                                                 'Hello world!', ...
+            %                                                                                 [], ...
+            %                                                                                 sim.simx_opmode_blocking);
+%}
+            
+% % % % %             Generate facilities using MATLAB functions
+% % %             Genarate a robot
+            [res_rob_genetate, rob_handle] = sim.simxLoadModel(clientID,'motoman_HP3J_with_base.ttm', 0, sim.simx_opmode_blocking);
+            [res_rob_setpos] = sim.simxSetObjectPosition(clientID, rob_handle, -1, [-0.6 0.15 0.15], sim.simx_opmode_oneshot);
+            
+% % %             Genarate a conveyor
+            [res_con_genetate, con_handle] = sim.simxLoadModel(clientID,'customizable conveyor belt_03x1x05.ttm', 0, sim.simx_opmode_blocking);
+            [res_con_setpos] = sim.simxSetObjectPosition(clientID, con_handle, -1, [0 0.5 0.45], sim.simx_opmode_oneshot);
+            [res_con_setorien] = sim.simxSetObjectOrientation(clientID, con_handle, -1, [0 0 -pi/2], sim.simx_opmode_oneshot)
+            
+% % %             Genarate a table
+            [res_tab_genetate, tab_handle] = sim.simxLoadModel(clientID,'customizable_table_with_create_cube_func.ttm', 0, sim.simx_opmode_blocking);
+            [res_tab_setpos] = sim.simxSetObjectPosition(clientID, tab_handle, -1, [-0.6 0.65 0.45], sim.simx_opmode_oneshot);
+            
+            pause(1);
+
+%             hndShape = sim.simxcreatePureShape(0,VISIBLE_EDGES+RESPONDABLE_SHAPE,tblSize,0.01,NULL)
+% tblSize={0.06,0.06,0.06}
+%                         hndShape = sim.simxCreatePureShape(0,10,tblSize,0.01,NULL)
+
+
+% % % %     create rectangular on conveyor                     
+ [res, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'createcube_function', ...
+                [1,1,1], ... %   [color_flag(0=NULL,1=green), 
+                [0, 0.9, 0.6, 0.1, 0.1, 0.05], ...  %   [posx, posy, posz, sizex, sizey, sizez]
+                '', ...
+                [], ...
+                sim.simx_opmode_blocking);
+            
+            disp(retInts);
+            
+% % % %     create cube on table                     
+ [res, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'createcube_function', ...
+                [0,1,1], ... %   [color_flag(0=NULL,1=green), 
+                [-0.6 0.65 0.6, 0.05, 0.05, 0.05], ...  %   [posx, posy, posz, sizex, sizey, sizez]
+                '', ...
+                [], ...
+                sim.simx_opmode_blocking);
+            [res_cube0_handle, cube0_handle] = sim.simxGetObjectHandle(clientID,'Cuboid0', sim.simx_opmode_blocking)
+            
+                         [res_cube1_handle, cube1_handle] = sim.simxGetObjectHandle(clientID,'Cuboid1', sim.simx_opmode_blocking)
+
+%                         [res_cube2_handle, cube2_handle] = sim.simxGetObjectHandle(clientID,'Cuboid2', sim.simx_opmode_blocking)
+
+            
+            
+% % % %     create rectangular on conveyor                     
+ [res, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'createcube_function', ...
+                [1,1,1], ... %   [color_flag(0=NULL,1=green), 
+                [1, 1, 0.3, 0.1, 0.1, 0.05], ...  %   [posx, posy, posz, sizex, sizey, sizez]
+                '', ...
+                [], ...
+                sim.simx_opmode_blocking);
+            
+            disp(retInts);
+            
+% % % %     create cube on table                     
+ [res, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+                'ResizableFloor_5_25', ...
+                sim.sim_scripttype_childscript, ...
+                'createcube_function', ...
+                [0,1,1], ... %   [color_flag(0=NULL,1=green), 
+                [1, 1, 0.3, 0.05, 0.05, 0.05], ...  %   [posx, posy, posz, sizex, sizey, sizez]
+                '', ...
+                [], ...
+                sim.simx_opmode_blocking);
+
+            
+            
+            
+            
+            
+             pause(3);
+            
+% % %             Remove a cube 
+            
+            [res_cube0_remove] = sim.simxRemoveObject(clientID, cube0_handle, sim.simx_opmode_blocking)
+            
+            [res_cube1_remove] = sim.simxRemoveObject(clientID, cube1_handle, sim.simx_opmode_blocking)
+% 
+%             [res_cube2_remove] = sim.simxRemoveObject(clientID, cube2_handle, sim.simx_opmode_blocking)
+%             
+%              [res_cube3_handle, cube3_handle] = sim.simxGetObjectHandle(clientID,'Cuboid3', sim.simx_opmode_blocking)
+%             [res_cube3_remove] = sim.simxRemoveObject(clientID, cube3_handle, sim.simx_opmode_blocking)
+
+
+% % %             Remove model (Matlab function)
+% % %             Remove a robot
+            [res_rob_remove] = sim.simxRemoveModel(clientID, rob_handle, sim.simx_opmode_blocking);
+            
+% % %             Remove a conveyor
+            [res_con_remove] = sim.simxRemoveModel(clientID, con_handle, sim.simx_opmode_blocking);
+
+% % %             Remove a table
+            [res_con_remove] = sim.simxRemoveModel(clientID, tab_handle, sim.simx_opmode_blocking);
+
+            
+            
+            
+            
+            
+            
+%{
+%             pause(1);
+% % % % % % Remove model (coppeliasim threaded child script function)
+            % % % % % % Remove manipulator model
+            fprintf('remove manipulator\n');
+%             [res_rob_remove, rob_handle] = sim.simxGetObjectHandle(clientID,'motoman_HP3J_base_link_respondable',sim.simx_opmode_blocking);
+            [rob_remove_flag] = sim.simxRemoveModel(clientID, rob_handle, sim.simx_opmode_blocking);
+            
+%             disp(res_rob_remove);
+            disp(rob_handle);
+            
+            fprintf('rob_remove_flag = \n');
+            disp(rob_remove_flag);
+            
+%             pause(1);
+            % % % % %         Remove table model
+            fprintf('remove customizableTable\n');
+            [res_tab_remove, tab_handle] = sim.simxGetObjectHandle(clientID,'customizableTable',sim.simx_opmode_blocking);
+            [tab_remove_flag] = sim.simxRemoveModel(clientID, tab_handle, sim.simx_opmode_blocking);
+            
+            disp(res_tab_remove);
+            disp(tab_handle);
+            
+            fprintf('tab_remove_flag = \n');
+            disp(tab_remove_flag);
+            
+            
+%             pause(1);
+            % % % %          Remove conveyor model
+            fprintf('remove customizableConveyor\n');
+            [res_con_remove, con_handle] = sim.simxGetObjectHandle(clientID,'customizableConveyor',sim.simx_opmode_blocking);
+            [con_remove_flag] = sim.simxRemoveModel(clientID, con_handle, sim.simx_opmode_blocking);
+            
+            disp(res_con_remove);
+            disp(con_handle);
+            
+            fprintf('con_remove_flag = \n');
+            disp(con_remove_flag);
+%}
+        end
+
+
+        
+        
+        
+
+                                                                            
+%         
+%         [res1, tab]=sim.simxGetObjectHandle(clientID,'customizable table.ttm',sim.simx_opmode_blocking);
+%                 pause(1);
+% %         codeA = sim.simxRemoveObject(clientID,rob,sim.simx_opmode_blocking);
+%         fprintf('ddddd\n');
+%         
+%         [returnCode, prop] = sim.simxGetModelProperty(clientID, rob, sim.simx_opmode_blocking)
+%         
+%         
+% %         codeAB = sim.simxSetModelProperty(clientID, tab, 61441, sim.simx_opmode_oneshot);
+%         %         simxmodelproperty_not_visible
+% %         [returnCode, prop] = sim.simxGetModelProperty(clientID, tab, sim.simx_opmode_blocking)
+% 
+%         [returnCode, position] = sim.simxGetObjectPosition(clientID,tab,-1,sim.simx_opmode_blocking)
+%         
+%         codeAC = sim.simxSetObjectPosition(clientID,tab,-1,[-1,2,1],sim.simx_opmode_blocking)
+% 
+%         [res1, con]=sim.simxGetObjectHandle(clientID,'customizable conveyor belt.ttm',sim.simx_opmode_blocking);
+%         codeB = sim.simxRemoveModel(clientID,con,sim.simx_opmode_blocking);
+%         
+%         [res1, tab]=sim.simxGetObjectHandle(clientID,'customizable table.ttm',sim.simx_opmode_blocking);
+%         codeC = sim.simxRemoveModel(clientID,tab,sim.simx_opmode_blocking);
+        
+        if (res_rob_genetate == sim.simx_return_ok)
+%             fprintf('Returned message: %s\n',retStrings);
+        else
+            fprintf('3333333Remote function call failed\n');
+        end
+%{
+        % 2. Now create a dummy object at coordinate 0.1,0.2,0.3 with name 'MyDummyName':
+        [res retInts retFloats retStrings retBuffer]=sim.simxCallScriptFunction(clientID,'spawn_test',sim.sim_scripttype_childscript,'createDummy_function',[],[0.1 0.2 0.3],'MyDummyName',[],sim.simx_opmode_blocking);
+        if (res==sim.simx_return_ok)
+            fprintf('Dummy handle: %d\n',retInts(1));
+        else
+            fprintf('Remote function call failed\n');
+        end
+
+        % 3. Now send a code string to execute some random functions:
+        code=['local octreeHandle=simCreateOctree(0.5,0,1)', char(10), ...
+            'simInsertVoxelsIntoOctree(octreeHandle,0,{0.1,0.1,0.1},{255,0,255})', char(10), ...
+            'return ''done'''];
+        [res retInts retFloats retStrings retBuffer]=sim.simxCallScriptFunction(clientID,'spawn_test',sim.sim_scripttype_childscript,'executeCode_function',[],[],code,[],sim.simx_opmode_blocking);
+        if (res==sim.simx_return_ok)
+            fprintf('Code execution returned: %s\n',retStrings);
+        else
+            fprintf('Remote function call failed\n');
+        end
+%}
+
+        % Now close the connection to CoppeliaSim:    
+        sim.simxFinish(clientID);
+    else
+        disp('Failed connecting to remote API server');
+    end
+    sim.delete(); % call the destructor!
+    
+    disp('Program ended');
+end
