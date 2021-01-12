467c21717de3dbd668e1f7c45b8bc20cd3c709ca
urago-ms <urago@ms.esys.tsukuba.ac.jp>
2021-01-13 08:39:10 +0900

    hoge

--- MATLAB-git/PickAndPlace_ver410/random_research_forum.m
+++ MATLAB-git/PickAndPlace_ver410/random_research_forum.m
@@ -1,204 +1,306 @@
-function [m_pos_random, p_pos_random, c_pos_random] = random_research()
-
-% old file name "optim_test_1_random_minmax_with_spawn_and_grasp_2.m"
-%clear
-
-% numnum = 10;
-% numnum_max = numnum;
-% sum = 0;
+% This example illustrates how to execute complex commands from
+% a remote API client. You can also use a similar construct for
+% commands that are not directly supported by the remote API.
 %
-% while numnum > 0
-
-
-%rng(0,'twister');   % randam number initialization
-rng('shuffle','twister');
-
-orange = '[0.9300 0.4431 0.1804]';
-d_green = '[0.4660 0.6740 0.1880]';
-% d_green = [0.4660 0.6740 0.1880];
-
-lim_x = 1000;  % space limit
-lim_y = 2000;  % space limit
-
-ml = 300; % manipulator base one side length
-pl = 400; % palette one side length
-
-link_1 = 260; % manipulator link1 length
-link_2 = 260; % manipulator link2 length
-
-cl_l = 1000;   % conveyor long side length
-cl_l_max = 1000;   % conveyor long side max length
-cl_l_min = 500;   % conveyor long side min length
-cl_s = 300;   % conveyor short side length
-
-% m_pos = zeros(1,2); % manipulator position
-% p_pos = zeros(1,2); % palette position
-% c_pos = zeros(1,2); % conveyor position
-%c_pos = [0 0.5];
-
-theta_1 = 0;    % Initial angle of J1 axis
-theta_2 = 0;    % Initial angle of J2 axis
-
-phi_1 = 0;    % Target angle of J1 axis
-phi_2 = 0;    % Target angle of J2 axis
-
-j1_max = pi;    % Maximum angle of J1 axis
-j2_max = 2/3*pi;    % Maximum angle of J2 axis
-
-omega_1 = 375;  % Max velocity of J1 axis[deg/s]
-omega_2 = 375;  % Max velocity of J2 axis[deg/s]
-
-% only for 120deg
-range_max = link_1 + link_2;  % Maximum manipulator range
-range_min = link_1;  % Minimum manipulator range
+% Load the demo scene 'remoteApiCommandServerExample.ttt' in CoppeliaSim, then
+% start the simulation and run this program.
+%
+% IMPORTANT: for each successful call to simxStart, there
+% should be a corresponding call to simxFinish at the end!
 
 
 
-
-m_pos_x = 0;
-p_pos_x = 0;
-c_pos_x = 0;
-
-m_pos_y = 0;
-p_pos_y = 0;
-c_pos_y = 1/2*cl_l;
-
-m_pos_z = 0;
-p_pos_z = 0;
-c_pos_z = 0;
-
-m_pos_random = [m_pos_x, m_pos_y, m_pos_z];
-p_pos_random = [p_pos_x, p_pos_y, p_pos_z];
-c_pos_random = [c_pos_x, c_pos_y, c_pos_z];
-
-
-
-in_pos = [0 0];     %inlet position
-out_pos = [0 1000];    %outlet position
-
-m_hand_pos = zeros(1,2); % manipulator hand position
-
-% m_min_range = sqrt(link_1^2 + link_2^2 - 2*link_1*link_2*cos(pi - j2_max)); % manipulator min range
-m_min_range = 188; % manipulator min range (radius)
-m_max_range = 495;  % Maximum manipulator range531 (radius)
-
-
-g_initial_theta_1 = 0;
-
-% Figure ID
-date = datetime;
-id_num = rem(second(datetime)*1000,1000);
-id_num_st = num2str(id_num);
-
-% Delete all figure
-close
-clc
-
-% Number of trials
-num_t = 10;
-max_num_t = num_t;
-interval = 5;
-
-num_best_angle = max_num_t/interval;
-best_min_angle_array = zeros(100000,1);
-
-
+function spawn_and_grasp()
 % Timer start
-tic
+tStart = tic
 
-% % % % while num_t > 0
 
-%%%%%% Manipulator position %%%%%%%%%%%%%%%%%%%%%%%%%%%
-while 1
-    %                 disp('test');
-    m_pos_x_s = -lim_x + (lim_x-(-lim_x))*rand;
-    m_pos_y_s = -lim_y + (lim_y-(-lim_y))*rand;
-    m_pos_x = round(m_pos_x_s);
-    m_pos_y = round(m_pos_y_s);
+disp('Program started');
+% sim=remApi('remoteApi','extApi.h'); % using the header (requires a compiler)
+sim = remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+sim.simxFinish(-1); % just in case, close all opened connections
+clientID = sim.simxStart('127.0.0.1',19998,true,true,5000,5);
+
+
+
+if (clientID>-1)
+    disp('Connected to remote API server');
+    
+    % % %     Simulation Start
+    [res_startSim] = sim.simxStartSimulation(clientID,sim.simx_opmode_blocking)
     
     
-    % The distance from the conveyor is within the movable range of the manipulator
-    if ((m_pos_x - c_pos_x)^2 + (m_pos_y - c_pos_y)^2 < m_max_range^2) && ((m_pos_x - c_pos_x)^2 + (m_pos_y - c_pos_y)^2 > m_min_range^2)
+    num_t = 1;
+    
+    
+    
+    cl_l = 2000;   % conveyor long side length
+    cl_s = 600;   % conveyor short side length
+    ml = 800; % manipulator base one side length
+    pl = 500; % palette one side length
+    m_min_range = 593; % manipulator min range (radius)
+    m_max_range = 2051;  % Maximum manipulator range531 (radius)
+    
+    m_min_range = 680; % manipulator min range (radius)
+    m_max_range = 2552;  % Maximum manipulator range531 (radius)
+    
+    
+    % % %             Genarate a conveyor
+    [res_con_genetate, con_handle] = sim.simxLoadModel(clientID,'customizable_conveyor_belt_06x2x08_fix_sensorPos.ttm', 0, sim.simx_opmode_blocking);
+    [res_con_setpos] = sim.simxSetObjectPosition(clientID, con_handle, -1, [0 1 0.75], sim.simx_opmode_oneshot);
+    [res_con_setorien] = sim.simxSetObjectOrientation(clientID, con_handle, -1, [0 0 -pi/2], sim.simx_opmode_oneshot);
+    
+    % % %     number of repetition
+    rep = 1000000;
+    
+    % % %         Repeated creation and deletion of the device.
+    for count = 1:rep
         
-        % Whether to overlap with the conveyor
-        if (m_pos_x > (c_pos_x + 1/2*cl_s + 1/2*ml)) || (m_pos_x < (c_pos_x - 1/2*cl_s - 1/2*ml))   % If it does not overlap in the X-axis direction
-            m_pos_x;
-            m_pos_y;
-            break;
+        % % % % %         Display the number of repetitions in CoppeliaSim
+        [res_print_repetition_rate, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'print_repetition_rate', ...
+            [num_t], ...
+            [], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % % % % %         Random search to locate manipulators and tables.
+        %         [m_pos_random, p_pos_random, c_pos_random] = random_research_forum();
+        %         rob_pos_2 = [m_pos_random(1), m_pos_random(2), 0.15];
+        %         tab_pos_2 = [p_pos_random(1), p_pos_random(2), 0.45];
+        
+        
+        [m_pos_random, p_pos_random, c_pos_random] = random_research(cl_l, cl_s, ml, pl, m_min_range, m_max_range);
+        rob_pos_2 = [m_pos_random(1), m_pos_random(2), 0.1];
+        tab_pos_2 = [p_pos_random(1), p_pos_random(2), 0.75];
+        
+        
+        % % % % %             Generate facilities using MATLAB functions % % % % %
+        % % %             Genarate a robot
+        %         [res_rob_genetate, rob_handle] = sim.simxLoadModel(clientID,'motoman_HP3J_with_base_JointsLimit2.ttm', 0, sim.simx_opmode_blocking);
+        %         [res_rob_setpos] = sim.simxSetObjectPosition(clientID, rob_handle, -1, rob_pos_2, sim.simx_opmode_oneshot);
+        
+        
+        % % %             Move robot position
+        [res_rob_handle, rob_handle] = sim.simxGetObjectHandle(clientID,'Cuboid', sim.simx_opmode_blocking);
+        [res_rob_setpos] = sim.simxSetObjectPosition(clientID, rob_handle, -1, rob_pos_2, sim.simx_opmode_oneshot);
+        
+        
+        % % %             Setting of target dummy
+        [res_target_gen, TargetDummyHandle] = sim.simxCreateDummy(clientID, 0.03, [], sim.simx_opmode_blocking);
+        [res_target_setpos] = sim.simxSetObjectPosition(clientID, TargetDummyHandle, rob_handle, [0.25, 0, 0.4], sim.simx_opmode_oneshot);
+        
+        % %     Set link dummy
+        [res_tip_dummy_handle, tip_dummy_handle] = sim.simxGetObjectHandle(clientID,'tip', sim.simx_opmode_blocking);
+        [res_set_link_dummy, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'set_link_dummy', ...
+            [tip_dummy_handle, TargetDummyHandle], ...
+            [], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % %     create IK group
+        [res_create_IK, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'create_IK_group', ...
+            [], ...
+            [], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        
+        % % %             Genarate a table
+        [res_tab_genetate, tab_handle] = sim.simxLoadModel(clientID,'customizable_table_with_create_cube_func_05_05_08.ttm', 0, sim.simx_opmode_blocking);
+        [res_tab_setpos] = sim.simxSetObjectPosition(clientID, tab_handle, -1, tab_pos_2, sim.simx_opmode_oneshot);
+        
+        
+        % % % % % %             Execute Pick and Place     % % % % % %
+        % % % Read the coordinates(position) of each facility.
+        [res_rob_pos, rob_pos] = sim.simxGetObjectPosition(clientID, rob_handle, -1, sim.simx_opmode_streaming);
+        [res_tab_pos, tab_pos] = sim.simxGetObjectPosition(clientID, tab_handle, -1, sim.simx_opmode_streaming);
+        [res_con_pos, con_pos] = sim.simxGetObjectPosition(clientID, con_handle, -1, sim.simx_opmode_streaming);
+        
+        
+        % % % % %             Generate Objects using CoppeliaSim functions % % % % %
+        % % % %     create rectangular on conveyor
+        [res_cube_gen_0, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'createcube_function', ...
+            [1,1,1], ... %   [color_flag(0=NULL,1=green),
+            [0, 0.9, 0.6, 0.1, 0.1, 0.05], ...  %   [posX, posY, posZ, sizeX, sizeY, sizeZ]
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % % % %     create cube on table
+        [res_cube_gen_1, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'createcube_function', ...
+            [0,1,1], ... %   [color_flag(0=NULL,1=green),
+            [tab_pos_2(1), tab_pos_2(2), tab_pos_2(3)+0.1, 0.05, 0.05, 0.05], ...  %   [posX, posY, posZ, sizeX, sizeY, sizeZ]
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        
+        [res_cube0_handle, cube0_handle] = sim.simxGetObjectHandle(clientID,'Cuboid0', sim.simx_opmode_blocking);
+        [res_cube1_handle, cube1_handle] = sim.simxGetObjectHandle(clientID,'Cuboid1', sim.simx_opmode_blocking);
+        
+        
+        % let's define now the target positions needed
+        fposition3 = [0, 0.52, 0.65,	0,	0,	0];    % above place position
+        fposition4 = [0, 0.52, 0.625,	0,	0,	0];   % place position
+        fposition5 = [tab_pos_2(1),	tab_pos_2(2),   0.65,    0,  0,  0];    % above pickup position
+        fposition6 = [tab_pos_2(1),	tab_pos_2(2),   tab_pos_2(3)+0.13,    0,  0,  0];    % pickup position
+        
+        
+        % Timer start
+        % tic
+        
+        % % %         Get the simtime on coppeliasim
+        [res_time, retInts_time, retFloats_time, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'get_simtime', ...
+            [],[], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % % %             Move the end effector to position 5.
+        moveL (clientID, TargetDummyHandle, fposition5, 8);
+        % % %             Move the end effector to position 6.
+        moveL (clientID, TargetDummyHandle, fposition6, 8);
+        
+        
+        % % %             Suck a cube
+        [res, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'suctionPad', ...
+            sim.sim_scripttype_childscript, ...
+            'suck_object', ...
+            [0,0,1],[0.1,0.3,0.68], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % % %             Move the end effector to position 5.
+        moveL (clientID, TargetDummyHandle, fposition5, 8);
+        % % %             Move the end effector to position 3.
+        moveL (clientID, TargetDummyHandle, fposition3, 8);
+        % % %             Move the end effector to position 4.
+        moveL (clientID, TargetDummyHandle, fposition4, 8);
+        
+        % % %             Release the cube.
+        [res, retInts, retFloats, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'suctionPad', ...
+            sim.sim_scripttype_childscript, ...
+            'release_object', ...
+            [0,0,1],[0.1,0.3,0.68], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % % %             Move the end effector to position 4.
+        moveL (clientID, TargetDummyHandle, fposition3, 8);
+        
+        % % %         Get the simtime on coppeliasim
+        [res_time2, retInts_time2, retFloats_time2, retStrings, retBuffer] = sim.simxCallScriptFunction(clientID, ...
+            'ResizableFloor_5_25', ...
+            sim.sim_scripttype_childscript, ...
+            'get_simtime', ...
+            [],[], ...
+            '', ...
+            [], ...
+            sim.simx_opmode_blocking);
+        
+        % % %             The time it took to perform the work
+        exeTime = retFloats_time2 - retFloats_time
+        
+        
+        % % %         Find the one with the shortest pick-and-place execution time among the generated placements.
+        % % %             Update Minimum exetime
+        if num_t == 1   %% if first time
+            disp('UPDATE min_exetime');
+            repetition_rate = num_t;
+            min_exeTime = exeTime;
+            optim_tabpos = tab_pos_2;
+            optim_robpos = rob_pos_2;
             
-        elseif (m_pos_y > (c_pos_y + 1/2*cl_l + 1/2*ml)) || (m_pos_y < (c_pos_y - 1/2*cl_l - 1/2*ml))   % If it does not overlap in the Y-axis direction
-            m_pos_x;
-            m_pos_y;
-            break;
+        elseif exeTime < min_exeTime
+            disp('UPDATE min_exetime');
+            repetition_rate = num_t;
+            min_exeTime = exeTime;
+            optim_tabpos = tab_pos_2;
+            optim_robpos = rob_pos_2;
+            
         else
         end
-    else
-    end
-end
-
-
-%%%%%% Palette position %%%%%%%%%%%%%%%%%%%%%%%%%%%
-while 1
-    p_pos_x_s = -lim_x + (lim_x-(-lim_x))*rand;
-    p_pos_y_s = -lim_y + (lim_y-(-lim_y))*rand;
-    p_pos_x = round(p_pos_x_s);
-    p_pos_y = round(p_pos_y_s);
-    
-    % The distance from the conveyor is within the movable range of the manipulator
-    if (p_pos_x - c_pos_x)^2 + (p_pos_y - c_pos_y)^2 < (2*m_max_range)^2
         
-        % The distance from the manipulator is within the movable range of the manipulator
-        if ((m_pos_x - p_pos_x)^2 + (m_pos_y - p_pos_y)^2 < m_max_range^2) && ((m_pos_x - p_pos_x)^2 + (m_pos_y - p_pos_y)^2 > m_min_range^2)
-            
-            % Whether to overlap with the palette
-            if (m_pos_x > (p_pos_x + 1/2*pl + 1/2*ml)) || (m_pos_x < (p_pos_x - 1/2*pl - 1/2*ml))   % If it does not overlap in the X-axis direction
-                
-                % Whether to overlap with the conveyor
-                if (p_pos_x > (c_pos_x + 1/2*cl_s + 1/2*pl)) || (p_pos_x < (c_pos_x - 1/2*cl_s - 1/2*pl))   % If it does not overlap in the X-axis direction
-                    p_pos_x;
-                    p_pos_y;
-                    %                         disp('Decide the position of the palette1111');
-                    break;
-                    
-                elseif (p_pos_y > (c_pos_y + 1/2*cl_l + 1/2*pl)) || (p_pos_y < (c_pos_y - 1/2*cl_l - 1/2*pl))   % If it does not overlap in the Y-axis direction
-                    p_pos_x;
-                    p_pos_y;
-                    %                         disp('Decide the position of the palette2222');
-                    break;
-                else
-                end
-                
-            elseif (m_pos_y > (p_pos_y + 1/2*pl + 1/2*ml)) || (m_pos_y < (p_pos_y - 1/2*pl - 1/2*ml))   % If it does not overlap in the Y-axis direction
-                
-                % Whether to overlap with the conveyor
-                if (p_pos_x > (c_pos_x + 1/2*cl_s + 1/2*pl)) || (p_pos_x < (c_pos_x - 1/2*cl_s - 1/2*pl))   % If it does not overlap in the X-axis direction
-                    p_pos_x;
-                    p_pos_y;
-                    %                         disp('Decide the position of the palette1111');
-                    break;
-                    
-                elseif (p_pos_y > (c_pos_y + 1/2*cl_l + 1/2*pl)) || (p_pos_y < (c_pos_y - 1/2*cl_l - 1/2*pl))   % If it does not overlap in the Y-axis direction
-                    p_pos_x;
-                    p_pos_y;
-                    %                         disp('Decide the position of the palette2222');
-                    break;
-                else
-                end
-            else
-            end
-            
-        else
-        end
-    else
+        % % %             Remove a TargetDummy
+        [res_targetdummy_remove] = sim.simxRemoveObject(clientID, TargetDummyHandle, sim.simx_opmode_blocking);
+        
+        % % %             Remove cubes
+        [res_cube0_remove] = sim.simxRemoveObject(clientID, cube0_handle, sim.simx_opmode_blocking);
+        [res_cube1_remove] = sim.simxRemoveObject(clientID, cube1_handle, sim.simx_opmode_blocking);
+        
+        
+        % % %             Remove model (Matlab function)  % % % % %
+        % % %             Remove a robot
+        [res_rob_remove] = sim.simxRemoveModel(clientID, rob_handle, sim.simx_opmode_blocking);
+        % % %             Remove a table
+        [res_con_remove] = sim.simxRemoveModel(clientID, tab_handle, sim.simx_opmode_blocking);
+        %         %}
+        
+        num_t = num_t + 1;
     end
+    
+    %     simulation pause
+    [res_sim_pause] = sim.simxPauseSimulation(clientID, sim.simx_opmode_oneshot);
+    
+    % % %             Remove a conveyor
+    [res_con_remove] = sim.simxRemoveModel(clientID, con_handle, sim.simx_opmode_blocking);
+    
+    fprintf('Optimized Table position:\n');
+    disp(optim_tabpos);
+    fprintf('Optimized Manipulator position:\n');
+    disp(optim_robpos);
+    %     fprintf('Optimized Manipulator position: %f\n', optim_robpos);
+    fprintf('Repetition Rate when Min Execute Time :%d\n', repetition_rate);
+    fprintf('Min Execute Time(sim time)[s] :%f\n', min_exeTime);
+    
+    % Timer stop[s]
+    elapsedTime = toc(tStart);
+    
+    fprintf('elapsedTime(real time)[s] :%f\n', elapsedTime);
+    
+    
+    if (res_rob_genetate == sim.simx_return_ok)
+        %             fprintf('Returned message: %s\n',retStrings);
+    else
+        fprintf('Remote function call failed\n');
+    end
+    
+    
+    % Now close the connection to CoppeliaSim:
+    sim.simxFinish(clientID);
+    
+else
+    disp('Failed connecting to remote API server');
 end
+sim.delete(); % call the destructor!
 
-%     [mm] -> [m]
-m_pos_random = [1/1000*m_pos_x, 1/1000*m_pos_y, 1/1000*m_pos_z];
-p_pos_random = [1/1000*p_pos_x, 1/1000*p_pos_y, 1/1000*p_pos_z];
-c_pos_random = [1/1000*c_pos_x, 1/1000*c_pos_y, 1/1000*c_pos_z];
+disp('Program ended');
 
-
+% whos
 
 end
-
